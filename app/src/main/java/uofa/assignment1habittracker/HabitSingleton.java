46429cd10ca466bdc3cbc90d69c8ede3034fa067
Andrei <drei3000@gmail.com>
2016-10-01 11:30:09 -0600

    working with son

--- app/src/main/java/uofa/assignment1habittracker/HabitSingleton.java
+++ app/src/main/java/uofa/assignment1habittracker/HabitSingleton.java
@@ -1,18 +1,34 @@
 package uofa.assignment1habittracker;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 /**
  * Created by drei on 2016-09-30.
  */
+
+/*
+    References:
+    - https://www.mkyong.com/java/how-do-convert-java-object-to-from-json-format-gson-api/
+
+ */
+
 public class HabitSingleton {
     private static HabitSingleton ourInstance = new HabitSingleton();
+
     private ArrayList<Habit> habitList = new ArrayList<Habit>();
     public static HabitSingleton getInstance() {
         return ourInstance;
     }
 
-    private HabitSingleton() {}
+    private HabitSingleton() {
+        loadHabits();
+    }
 
     public void addHabit(Habit habit) {
         this.habitList.add(habit);
@@ -28,5 +44,21 @@ public class HabitSingleton {
 
     public Habit getHabitAtIndex(int position) {
         return habitList.get(position);
+    }
+
+    private void loadHabits() {
+        Type typeList = new TypeToken<ArrayList<Habit>>(){}.getType();
+        String jsonString = new Gson().toJson(habitList, typeList);
+        habitList = new Gson().fromJson(jsonString, typeList);
+    }
+
+    private void pushHabit(Habit habit) throws IOException {
+        Gson gson = new Gson();
+        FileWriter fileWriter = new FileWriter("D://habits.json");
+        gson.toJson(habit, fileWriter);
+    }
+
+    private void pullHabit() {
+
     }
 }
