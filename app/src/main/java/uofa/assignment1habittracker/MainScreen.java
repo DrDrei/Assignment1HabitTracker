192938460b4cd89baa5ae6e2e0c6f83c56f5b055
Andrei <drei3000@gmail.com>
2016-10-01 21:02:56 -0600

    clean up - Working on passing object instead of an integer in intent

--- app/src/main/java/uofa/assignment1habittracker/MainScreen.java
+++ app/src/main/java/uofa/assignment1habittracker/MainScreen.java
@@ -14,12 +14,9 @@ import android.widget.TextView;
 public class MainScreen extends Activity {
 
     private ListView habitListView;
-    private TextView rowTextView;
 
     private TextView headerTitleText;
 
-    private Button incRowButton;
-    private Button decRowButton;
 
     private Button addHabitButton;
     private ArrayAdapter<Habit> habitArrayAdapter;
@@ -28,17 +25,13 @@ public class MainScreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
-        incRowButton = (Button) findViewById(R.id.incRowButton);
-        decRowButton = (Button) findViewById(R.id.decRowButton);
-        rowTextView = (TextView) findViewById(R.id.rowTextView);
-        habitListView = (ListView) findViewById(android.R.id.list);
 
+        habitListView = (ListView) findViewById(android.R.id.list);
 
         configureHeader();
         HabitSingleton.getInstance().loadHabits(getApplicationContext());
 
         addHabitButton.setOnClickListener(new View.OnClickListener() {
-
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), AddHabitActivity.class);
                 startActivityForResult(intent, 0);
